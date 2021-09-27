3fc6566ee287b8dd2fcb290df7d69bcaf5bed77d
sktanwar2014 <sktanwar.2014@gmail.com>
2021-09-27 16:33:01 +0530

    fd

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+import Dashboard from './pages/dashboard';
+import Client from './pages/client';
+
+function App() {
+  return (
+    <Client />
+  );
+}
+
+export default App;
